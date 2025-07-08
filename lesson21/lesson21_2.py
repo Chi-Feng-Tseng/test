f45c4cfedfbd6a2e37e380b885166208c279ac9a
chifeng <tcf651004@gmail.com>
2025-07-08 19:49:24 +0800

    lesson21_2_1

--- lesson21/lesson21_2.py
+++ lesson21/lesson21_2.py
@@ -4,13 +4,13 @@ def on_button_click():
     print("Hello, Tkinter!")
 
 def create_main_window():
-    window = tk.Tk()
-    window.title("簡單Tkinter範例")
-    window.geometry("300x200")
+    window = tk.Tk()                #建立主視窗
+    window.title("簡單Tkinter範例")  #建立視窗標題
+    window.geometry("500x500")      #設定視窗大小
 
     # 建立按鈕並綁定事件處理函式
-    btn = tk.Button(window, text="點我", command=on_button_click)
-    btn.pack(pady=50)
+    btn = tk.Button(window, text="點我", command=on_button_click, padx=20, pady=20, font=("Arial", 16))
+    btn.pack(pady=100)
 
     return window
 
