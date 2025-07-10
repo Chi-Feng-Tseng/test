1e58c81c261dae193ab465d592f1b5cab10cf720
chifeng <tcf651004@gmail.com>
2025-07-10 19:42:59 +0800

    main

--- lesson21_web/main.py
+++ lesson21_web/main.py
@@ -6,15 +6,14 @@ app = Flask(__name__)
 
 @app.route("/")
 @app.route("/<string:question>")
-def index(question:string=""):
-    if question=="" :
-
+def index(question:str=''):
+    if question=='':
         return "<h1>我是Gemini的小助手</h1>"
     else:
         client = genai.Client()
         response = client.models.generate_content(
-         model = "gemini-2.5-flash", contents = f"{question},回應請輸出成為heml格式"
-        )    
+            model="gemini-2.5-flash", contents=f"{question},回應請輸出成為html格式"
+        )
         html_format = response.text
         html_format = response.text.replace("```html","").replace("```","")
         return html_format
